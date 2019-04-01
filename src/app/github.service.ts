9ad167a70051ccd07313be622819608ed796cf7b
stas <astatsegre@gmail.com>
2019-04-01 11:04:53 +0300

    added encodeURIComponent

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -12,7 +12,7 @@ export class GithubService {
 
   constructor(private httpClient: HttpClient) { }
   public getReposList(query: string): Observable<HandledResponse> {
-    return this.httpClient.get<GithubSearchInterface>(`https://api.github.com/search/repositories?q=${query}+in:name`)
+    return this.httpClient.get<GithubSearchInterface>(`https://api.github.com/search/repositories?q=${encodeURIComponent(query)}+in:name`)
       .pipe(
         map((resp) => {
           return {items: resp.items, error: null};
